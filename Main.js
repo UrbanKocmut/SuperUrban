e1c212531a5f30fe58d77157753aafef93c94495
UrbanKocmut <u.kocmut@gmail.com>
2017-04-08 12:02:39 +0200

    Donan timer, črta, škatla, zazanvanje inputa

--- Main.js
+++ Main.js
@@ -1,16 +1,84 @@
 'use strict';
 var canvas;
+var timer = {
+    time: 0,
+    x: 350,
+    y: 10,
+    set: function () {
+        timer.time = performance.now();
+    },
+    draw: function () {
+        var diff = performance.now() - timer.time;
+        canvas.fillText(String(diff), timer.x, timer.y);
+    }
+};
+var box = {
+    x: 50,
+    y: 50,
+    width: 20,
+    height: 20,
+    draw: function () {
+        canvas.fillRect(box.x, box.y, box.width, box.height);
+    }
+};
+var line = {
+    y: 200,
+    draw: function () {
+        canvas.beginPath();
+        canvas.moveTo(0, line.y);
+        canvas.lineTo(canvas.WIDTH, line.y);
+        canvas.stroke();
+    }
+};
+var text = {
+    x: 10,
+    y: 10,
+    text: "NI",
+    draw: function () {
+        canvas.fillText(text.text, text.x, text.y);
+    }
+};
 
 function init() {
     canvas = document.getElementById("canvas").getContext("2d");
+    canvas.WIDTH = canvas.canvas.width;
+    canvas.HEIGHT = canvas.canvas.height;
+}
+
+function draw() {
+    canvas.clearRect(0, 0, canvas.WIDTH, canvas.HEIGHT);
+    line.draw();
+    box.draw();
+    text.draw();
 }
 
 function update() {
+    timer.set();
+    draw();
+    timer.draw();
     window.requestAnimationFrame(update);
 }
 
+
+function keyHandler(event) {
+    switch (event.keyCode) {
+        case 37:
+            text.text = "LEFT";
+            break;
+        case 38:
+            text.text = "UP";
+            break;
+        case 39:
+            text.text = "RIGHT";
+            break;
+        case 40:
+            text.text = "DOWN";
+            break;
+    }
+}
 
 window.onload = function () {
     init();
     update();
 }
+window.addEventListener("keydown", keyHandler);
